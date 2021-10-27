4f7b90785c9a3d691f0356c9d87935cb566cb090
TareqAhsan <tareqahsan61@gmail.com>
2021-10-27 21:29:34 +0600

    changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const { MongoClient } = require("mongodb");
 const ObjectId = require('mongodb').ObjectId
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 // middle ware 
 app.use(cors())
 app.use(express.json())
